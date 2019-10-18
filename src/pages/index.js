6ef90a878f0a23c22452961f75bd56c73e96de5c
Rodave Joseph Bobadilla <rbobadilla@isbx.com>
2019-10-18 16:02:10 +0800

    tantararaaaan!

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,481 @@
 import React from "react"
-import { Link } from "gatsby"
+// import "../css/fontawesome-all.min.css"
+import "../sass/main.scss"
+import Helmet from "react-helmet"
+import { withPrefix } from "gatsby"
+import Avatar from "react-avatar"
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component"
+import "react-vertical-timeline-component/style.min.css"
+import SkillBar from "react-skillbars"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+// https://html5up.net/hyperspace
+
+const SKILLS = [
+  {
+    type: "Java",
+    level: 100,
+  },
+  {
+    type: "React",
+    level: 85,
+  },
+  {
+    type: "Javascript",
+    level: 75,
+  },
+  {
+    type: "Spring",
+    level: 50,
+  },
+  {
+    type: "Docker",
+    level: 25,
+  },
+  {
+    type: "HTML",
+    level: 20,
+  },
+  {
+    type: "NoSQL",
+    level: 0,
+  },
+]
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+  <div>
+    {/* Sidebar */}
+    <section id="sidebar">
+      <div className="inner">
+        <nav>
+          <ul>
+            <li>
+              <a href="#intro">Welcome</a>
+            </li>
+            <li>
+              <a href="#one">Soft Skills</a>
+            </li>
+            <li>
+              <a href="#two">Hard Skills</a>
+            </li>
+            <li>
+              <a href="#three">Professional Experience</a>
+            </li>
+            <li>
+              <a href="#three">Seminars Attended</a>
+            </li>
+            <li>
+              <a href="#three">My craft</a>
+            </li>
+            <li>
+              <a href="#three">Education</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </section>
+    {/* Wrapper */}
+    <div id="wrapper">
+      {/* Intro */}
+      <section id="intro" className="wrapper style1 fullscreen fade-up">
+        <Avatar
+          className="avatar"
+          src="/images/avatar.png"
+          size="200"
+          round={true}
+          alt="full-stack software developer Rodave Joseph Bobadilla"
+        />
+        <div className="inner">
+          <h1>Rodave Joseph B. Bobadilla</h1>
+          <p>
+            Seeking a challenging career with a progressive organization that
+            provides an opportunity to capitalize my technical skills &
+            abilities in the field of web application development.
+          </p>
+          <ul className="actions">
+            <li>
+              <a href="#one" className="button scrolly">
+                Learn more
+              </a>
+            </li>
+          </ul>
+        </div>
+      </section>
+
+      <section id="hard" className="wrapper style3 fade-up">
+        <div className="inner">
+          <SkillBar skills={SKILLS} height={50} />
+        </div>
+      </section>
+
+      {/* One */}
+      <section id="one" className="wrapper style2 spotlights">
+        <section>
+          <a href="#" className="image">
+            <img src="images/pic01.jpg" alt="" data-position="center center" />
+          </a>
+          <div className="content">
+            <div className="inner">
+              <h2>Sed ipsum dolor</h2>
+              <p>
+                Phasellus convallis elit id ullamcorper pulvinar. Duis aliquam
+                turpis mauris, eu ultricies erat malesuada quis. Aliquam
+                dapibus.
+              </p>
+              <ul className="actions">
+                <li>
+                  <a href="generic.html" className="button">
+                    Learn more
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </section>
+        <section>
+          <a href="#" className="image">
+            <img src="images/pic02.jpg" alt="" data-position="top center" />
+          </a>
+          <div className="content">
+            <div className="inner">
+              <h2>Feugiat consequat</h2>
+              <p>
+                Phasellus convallis elit id ullamcorper pulvinar. Duis aliquam
+                turpis mauris, eu ultricies erat malesuada quis. Aliquam
+                dapibus.
+              </p>
+              <ul className="actions">
+                <li>
+                  <a href="generic.html" className="button">
+                    Learn more
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </section>
+        <section>
+          <a href="#" className="image">
+            <img src="images/pic03.jpg" alt="" data-position="25% 25%" />
+          </a>
+          <div className="content">
+            <div className="inner">
+              <h2>Ultricies aliquam</h2>
+              <p>
+                Phasellus convallis elit id ullamcorper pulvinar. Duis aliquam
+                turpis mauris, eu ultricies erat malesuada quis. Aliquam
+                dapibus.
+              </p>
+              <ul className="actions">
+                <li>
+                  <a href="generic.html" className="button">
+                    Learn more
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </section>
+      </section>
+      {/* Timeline */}
+      <section id="three" className="wrapper style1 fade-up">
+        <VerticalTimeline>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            contentStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+            contentArrowStyle={{ borderRight: "7px solid  rgb(33, 150, 243)" }}
+            date="2011 - present"
+            iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+          >
+            <h3 className="vertical-timeline-element-title">
+              Creative Director
+            </h3>
+            <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
+            <p>
+              Creative Direction, User Experience, Visual Design, Project
+              Management, Team Leading
+            </p>
+          </VerticalTimelineElement>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            date="2010 - 2011"
+            iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+          >
+            <h3 className="vertical-timeline-element-title">Art Director</h3>
+            <h4 className="vertical-timeline-element-subtitle">
+              San Francisco, CA
+            </h4>
+            <p>
+              Creative Direction, User Experience, Visual Design, SEO, Online
+              Marketing
+            </p>
+          </VerticalTimelineElement>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            date="2008 - 2010"
+            iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+          >
+            <h3 className="vertical-timeline-element-title">Web Designer</h3>
+            <h4 className="vertical-timeline-element-subtitle">
+              Los Angeles, CA
+            </h4>
+            <p>User Experience, Visual Design</p>
+          </VerticalTimelineElement>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            date="2006 - 2008"
+            iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+          >
+            <h3 className="vertical-timeline-element-title">Web Designer</h3>
+            <h4 className="vertical-timeline-element-subtitle">
+              San Francisco, CA
+            </h4>
+            <p>User Experience, Visual Design</p>
+          </VerticalTimelineElement>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--education"
+            date="April 2013"
+            iconStyle={{ background: "rgb(233, 30, 99)", color: "#fff" }}
+          >
+            <h3 className="vertical-timeline-element-title">
+              Content Marketing for Web, Mobile and Social Media
+            </h3>
+            <h4 className="vertical-timeline-element-subtitle">
+              Online Course
+            </h4>
+            <p>Strategy, Social Media</p>
+          </VerticalTimelineElement>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--education"
+            date="November 2012"
+            iconStyle={{ background: "rgb(233, 30, 99)", color: "#fff" }}
+          >
+            <h3 className="vertical-timeline-element-title">
+              Agile Development Scrum Master
+            </h3>
+            <h4 className="vertical-timeline-element-subtitle">
+              Certification
+            </h4>
+            <p>Creative Direction, User Experience, Visual Design</p>
+          </VerticalTimelineElement>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--education"
+            date="2002 - 2006"
+            iconStyle={{ background: "rgb(233, 30, 99)", color: "#fff" }}
+          >
+            <h3 className="vertical-timeline-element-title">
+              Bachelor of Science in Interactive Digital Media Visual Imaging
+            </h3>
+            <h4 className="vertical-timeline-element-subtitle">
+              Bachelor Degree
+            </h4>
+            <p>Creative Direction, Visual Design</p>
+          </VerticalTimelineElement>
+          <VerticalTimelineElement
+            iconStyle={{ background: "rgb(16, 204, 82)", color: "#fff" }}
+          />
+        </VerticalTimeline>
+      </section>
+
+      {/* Two */}
+      <section id="two" className="wrapper style3 fade-up">
+        <div className="inner">
+          <h2>What we do</h2>
+          <p>
+            Phasellus convallis elit id ullamcorper pulvinar. Duis aliquam
+            turpis mauris, eu ultricies erat malesuada quis. Aliquam dapibus,
+            lacus eget hendrerit bibendum, urna est aliquam sem, sit amet
+            imperdiet est velit quis lorem.
+          </p>
+          <div className="features">
+            <section>
+              <span className="icon solid major fa-code" />
+              <h3>Lorem ipsum amet</h3>
+              <p>
+                Phasellus convallis elit id ullam corper amet et pulvinar. Duis
+                aliquam turpis mauris, sed ultricies erat dapibus.
+              </p>
+            </section>
+            <section>
+              <span className="icon solid major fa-lock" />
+              <h3>Aliquam sed nullam</h3>
+              <p>
+                Phasellus convallis elit id ullam corper amet et pulvinar. Duis
+                aliquam turpis mauris, sed ultricies erat dapibus.
+              </p>
+            </section>
+            <section>
+              <span className="icon solid major fa-cog" />
+              <h3>Sed erat ullam corper</h3>
+              <p>
+                Phasellus convallis elit id ullam corper amet et pulvinar. Duis
+                aliquam turpis mauris, sed ultricies erat dapibus.
+              </p>
+            </section>
+            <section>
+              <span className="icon solid major fa-desktop" />
+              <h3>Veroeros quis lorem</h3>
+              <p>
+                Phasellus convallis elit id ullam corper amet et pulvinar. Duis
+                aliquam turpis mauris, sed ultricies erat dapibus.
+              </p>
+            </section>
+            <section>
+              <span className="icon solid major fa-link" />
+              <h3>Urna quis bibendum</h3>
+              <p>
+                Phasellus convallis elit id ullam corper amet et pulvinar. Duis
+                aliquam turpis mauris, sed ultricies erat dapibus.
+              </p>
+            </section>
+            <section>
+              <span className="icon major fa-gem" />
+              <h3>Aliquam urna dapibus</h3>
+              <p>
+                Phasellus convallis elit id ullam corper amet et pulvinar. Duis
+                aliquam turpis mauris, sed ultricies erat dapibus.
+              </p>
+            </section>
+          </div>
+          <ul className="actions">
+            <li>
+              <a href="generic.html" className="button">
+                Learn more
+              </a>
+            </li>
+          </ul>
+        </div>
+      </section>
+      {/* Three */}
+      <section id="none" className="wrapper style1 fade-up">
+        <div className="inner">
+          <h2>Get in touch</h2>
+          <p>
+            Phasellus convallis elit id ullamcorper pulvinar. Duis aliquam
+            turpis mauris, eu ultricies erat malesuada quis. Aliquam dapibus,
+            lacus eget hendrerit bibendum, urna est aliquam sem, sit amet
+            imperdiet est velit quis lorem.
+          </p>
+          <div className="split style1">
+            <section>
+              <form method="post" action="#">
+                <div className="fields">
+                  <div className="field half">
+                    <label htmlFor="name">Name</label>
+                    <input type="text" name="name" id="name" />
+                  </div>
+                  <div className="field half">
+                    <label htmlFor="email">Email</label>
+                    <input type="text" name="email" id="email" />
+                  </div>
+                  <div className="field">
+                    <label htmlFor="message">Message</label>
+                    <textarea
+                      name="message"
+                      id="message"
+                      rows={5}
+                      defaultValue={""}
+                    />
+                  </div>
+                </div>
+                <ul className="actions">
+                  <li>
+                    <a href className="button submit">
+                      Send Message
+                    </a>
+                  </li>
+                </ul>
+              </form>
+            </section>
+            <section>
+              <ul className="contact">
+                <li>
+                  <h3>Address</h3>
+                  <span>
+                    12345 Somewhere Road #654
+                    <br />
+                    Nashville, TN 00000-0000
+                    <br />
+                    USA
+                  </span>
+                </li>
+                <li>
+                  <h3>Email</h3>
+                  <a href="#">user@untitled.tld</a>
+                </li>
+                <li>
+                  <h3>Phone</h3>
+                  <span>(000) 000-0000</span>
+                </li>
+                <li>
+                  <h3>Social</h3>
+                  <ul className="icons">
+                    <li>
+                      <a href="#" className="icon brands fa-twitter">
+                        <span className="label">Twitter</span>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#" className="icon brands fa-facebook-f">
+                        <span className="label">Facebook</span>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#" className="icon brands fa-github">
+                        <span className="label">GitHub</span>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#" className="icon brands fa-instagram">
+                        <span className="label">Instagram</span>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#" className="icon brands fa-linkedin-in">
+                        <span className="label">LinkedIn</span>
+                      </a>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </section>
+          </div>
+        </div>
+      </section>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+    {/* Footer */}
+    <footer id="footer" className="wrapper style1-alt">
+      <div className="inner">
+        <ul className="menu">
+          <li>© Untitled. All rights reserved.</li>
+          <li>
+            Design: <a href="http://html5up.net">HTML5 UP</a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+    {/* Scripts */}
+    <Helmet>
+      <script src={withPrefix("js/jquery.min.js")} type="text/javascript" />
+      <script
+        src={withPrefix("js/jquery.scrollex.min.js")}
+        type="text/javascript"
+      />
+      <script
+        src={withPrefix("js/jquery.scrolly.min.js")}
+        type="text/javascript"
+      />
+      <script src={withPrefix("js/browser.min.js")} type="text/javascript" />
+      <script
+        src={withPrefix("js/breakpoints.min.js")}
+        type="text/javascript"
+      />
+      <script src={withPrefix("js/util.js")} type="text/javascript" />
+      <script src={withPrefix("js/main.js")} type="text/javascript" />
+    </Helmet>
+  </div>
 )
 
 export default IndexPage
