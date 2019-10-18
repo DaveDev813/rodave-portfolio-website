8841b6e3cdb552d4ac6f68759a106f20fd550035
Rodave Joseph Bobadilla <rbobadilla@isbx.com>
2019-10-18 21:08:14 +0800

    clean

--- src/components/sections/proff.js
+++ src/components/sections/proff.js
@@ -3,6 +3,7 @@ import {
   VerticalTimeline,
   VerticalTimelineElement,
 } from "react-vertical-timeline-component"
+import "react-vertical-timeline-component/style.min.css"
 
 const Proff = () => (
   <section id="three" className="wrapper style1 fade-up">
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,14 @@
 import React from "react"
-// import "../css/fontawesome-all.min.css"
-import "../sass/main.scss"
-
-import {
-  VerticalTimeline,
-  VerticalTimelineElement,
-} from "react-vertical-timeline-component"
-import "react-vertical-timeline-component/style.min.css"
-import SkillBar from "react-skillbars"
-import "../css/fontawesome-all.min.css"
-import "../css/noscript.css"
 import SoftSkills from "../components/sections/softSkills"
 import Footer from "../components/layout/footer"
 import SideBar from "../components/layout/sideBar"
 import Intro from "../components/sections/intro"
 import HardSkills from "../components/sections/hardSkills"
 import Proff from "../components/sections/proff"
+
+import "../sass/main.scss"
+import "../css/fontawesome-all.min.css"
+import "../css/noscript.css"
 
 // https://html5up.net/hyperspace
 
