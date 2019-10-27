89665b0f8194b146a70650dcbd10f8f4778f0d83
DaveDev13 <bobadilla.rodave@gmail.com>
2019-10-27 20:38:39 +0800

    componentalize seminars

--- src/components/sections/seminars.js
+++ src/components/sections/seminars.js
@@ -2,8 +2,57 @@ import React from "react"
 import ModalVideo from "react-modal-video"
 import "react-modal-video/scss/modal-video.scss"
 
+const SeminarItem = ({ videoId, img, imgAlt = "", title, desc, learnMore }) => {
+  const [yt1, setYt1] = React.useState(false)
+  return (
+    <section>
+      {videoId && (
+        <ModalVideo
+          channel="youtube"
+          isOpen={yt1}
+          videoId={videoId}
+          onClose={() => setYt1(false)}
+          youtube={{ autoplay: true }}
+        />
+      )}
+      <div className="image" style={{ display: "flex" }}>
+        <img
+          className="thumbImage"
+          src={img}
+          alt={imgAlt}
+          data-position="center center"
+        />
+      </div>
+      <div className="content">
+        <div className="inner">
+          <h2>{title}</h2>
+          <p>{desc}</p>
+          <ul className="actions">
+            {videoId && (
+              <li>
+                <button
+                  className="button icon solid fa-play"
+                  onClick={() => setYt1(true)}
+                >
+                  Play video
+                </button>
+              </li>
+            )}
+            <li>
+              <a href={learnMore} className="button">
+                Learn more
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </section>
+  )
+}
+
 const Seminars = () => {
   const [yt1, setYt1] = React.useState(false)
+  //  videoId, img, imgAlt = "", title, desc, learnMore }=
   return (
     <section id="seminar" className="wrapper style3 spotlights">
       {/* <section style={{ minHeight: "auto", width: "100%" }}>
@@ -14,115 +63,13 @@ const Seminars = () => {
         </div>
       </section> */}
 
-      <section>
-        <ModalVideo
-          channel="youtube"
-          isOpen={yt1}
-          videoId="Fa2IO6A7KjE"
-          onClose={() => setYt1(false)}
-          youtube={{ autoplay: true }}
-        />
-        <div className="image" style={{ display: "flex" }}>
-          <img
-            className="thumbImage"
-            src="images/deeevcon.png"
-            alt=""
-            data-position="center center"
-          />
-        </div>
-        <div className="content">
-          <div className="inner">
-            <h2>DevCon Summit 2019</h2>
-            <p>
-              The Future of Jobs, Embracing Industry 4.0”. The Summit is a
-              platform for developers and industry leaders to hear insightful
-              keynote speakers, see unique exhibits, and experience highly
-              curated sessions that are designed for the Filipino computer
-              programmers.
-            </p>
-            <ul className="actions">
-              <li>
-                <button
-                  className="button icon solid fa-play"
-                  onClick={() => setYt1(true)}
-                >
-                  Play video
-                </button>
-              </li>
-              <li>
-                <a href="https://summit.devcon.ph" className="button">
-                  Learn more
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </section>
-
-      {/* <section>
-        <a href="#" className="image">
-          <img src="images/pic02.jpg" alt="" data-position="top center" />
-        </a>
-        <div className="content">
-          <div className="inner">
-            <h2>Feugiat consequat</h2>
-            <p>
-              Phasellus convallis elit id ullamcorper pulvinar. Duis aliquam
-              turpis mauris, eu ultricies erat malesuada quis. Aliquam dapibus.
-            </p>
-            <ul className="actions">
-              <li>
-                <a href="generic.html" className="button">
-                  Learn more
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </section>
-      <section>
-        <a href="#" className="image">
-          <img src="images/pic03.jpg" alt="" data-position="25% 25%" />
-        </a>
-        <div className="content">
-          <div className="inner">
-            <h2>Ultricies aliquam</h2>
-            <p>
-              Phasellus convallis elit id ullamcorper pulvinar. Duis aliquam
-              turpis mauris, eu ultricies erat malesuada quis. Aliquam dapibus.
-            </p>
-            <ul className="actions">
-              <li>
-                <a href="generic.html" className="button">
-                  Learn more
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </section>
-
-      <section>
-        <a href="#" className="image">
-          <img src="images/pic03.jpg" alt="" data-position="25% 25%" />
-        </a>
-        <div className="content">
-          <div className="inner">
-            <h2>Ultricies aliquam</h2>
-            <p>
-              Phasellus convallis elit id ullamcorper pulvinar. Duis aliquam
-              turpis mauris, eu ultricies erat malesuada quis. Aliquam dapibus.
-            </p>
-            <ul className="actions">
-              <li>
-                <a href="generic.html" className="button">
-                  Learn more
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </section> */}
+      <SeminarItem
+        videoId="Fa2IO6A7KjE"
+        img="images/deeevcon.png"
+        title="DevCon Summit 2019"
+        desc="The Future of Jobs, Embracing Industry 4.0”. The Summit is a platform for developers and industry leaders to hear insightful keynote speakers, see unique exhibits, and experience highly curated sessions that are designed for the Filipino computer"
+        learnMore="https://summit.devcon.ph"
+      />
     </section>
   )
 }
