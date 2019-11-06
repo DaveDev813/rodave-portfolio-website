4e8291f71fdeaae63fe4be1fa1f057bbf2967f35
Rodave Joseph Bobadilla <rbobadilla@isbx.com>
2019-11-06 12:58:50 +0800

    add images for works

--- src/components/sections/works.js
+++ src/components/sections/works.js
@@ -20,13 +20,34 @@ const Works = () => {
       learn: "#",
     },
     {
-      img:
-        "https://images.unsplash.com/photo-1519999482648-25049ddd37b1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2126&q=80",
-      title: "Lorem ipsum",
+      img: "images/works/sierra.png",
+      title: "Sierra Labs",
       desc:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta dolor praesentium at quod autem omnis",
+        "HOLLIBLU is consistently dedicated to creating a supportive and thriving healthcare ecosystem. We believe that when nurses thrive so does ...",
       learn: "#",
     },
+    {
+      img: "images/works/redbullwing.png",
+      title: "Redbull",
+      desc:
+        "HOLLIBLU is consistently dedicated to creating a supportive and thriving healthcare ecosystem. We believe that when nurses thrive so does ...",
+      learn: "#",
+    },
+    {
+      img: "images/works/gd.png",
+      title: "Green Direct",
+      desc:
+        "HOLLIBLU is consistently dedicated to creating a supportive and thriving healthcare ecosystem. We believe that when nurses thrive so does ...",
+      learn: "#",
+    },
+    // {
+    //   img:
+    //     "https://images.unsplash.com/photo-1519999482648-25049ddd37b1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2126&q=80",
+    //   title: "Lorem ipsum",
+    //   desc:
+    //     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta dolor praesentium at quod autem omnis",
+    //   learn: "#",
+    // },
   ]
   let delay = 0
   items = items.map(function(item, index) {
