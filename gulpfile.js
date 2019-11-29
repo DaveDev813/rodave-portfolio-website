909b12cb84075084677cceed13a7b68bd3fafc5a
Rodave Joseph Bobadilla <rbobadilla@isbx.com>
2019-11-29 11:09:30 +0800

    Add raw static files to be build by gulp and for optimisation

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,12 @@ var gulp = require("gulp")
 var uglify = require("gulp-uglify")
 var pipeline = require("readable-stream").pipeline
 var concat = require("gulp-concat")
+const imagemin = require("gulp-imagemin")
 
-var jsSrc = "src/staticScripts/*.js"
+var jsSrc = "src/staticSrc/scripts/*.js"
+var imgSrc = "src/staticSrc/images/**/*"
 
-gulp.task("compress", function() {
+gulp.task("compressjs", function() {
   return pipeline(
     gulp.src(jsSrc),
     concat("compressed.js"),
@@ -14,6 +16,10 @@ gulp.task("compress", function() {
   )
 })
 
+gulp.task("optimizeimages", function() {
+  return pipeline(gulp.src(imgSrc), imagemin(), gulp.dest("static/images/"))
+})
+
 gulp.task("default", function() {
-  gulp.watch([jsSrc], ["compress"])
+  gulp.watch([jsSrc], ["compressjs"])
 })
